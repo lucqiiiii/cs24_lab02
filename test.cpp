47095d3fe85bd70b63fda262cd716f6ad6349dd2
lucqiiiii <qi_guo@umail.ucsb.edu>
2018-10-11 15:15:50 -0700

    done

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void f(){
+  clock_t begin = clock();
+  int sum = 0;
+  int n = 1000;
+  for (int i = 0; i < n; ++i){
+    for (int j = 0; j < n; ++j){
+      ++sum;
+    }
+  }
+  clock_t end = clock();
+  double diff = double(end - begin) / CLOCKS_PER_SEC;
+  cout << diff << endl;
+}
+
+int main(){
+  f();
+  return 0;
+}
